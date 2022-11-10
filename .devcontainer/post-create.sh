4decd40acb0c2a4439c796f84c72896286e5a16c
3x!1 <exilvm@gmail.com>
2022-11-10 18:35:38 +0000

    Move `post-create` into a seperate file

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,13 @@
+sudo apt update -y
+sudo apt upgrade -y 
+sudo apt install yum yum-utils -y 
+mkdir -p /etc/pki/rpm-gpg
+wget https://yum.oracle.com/RPM-GPG-KEY-oracle-ol7 -O /etc/pki/rpm-gpg/RPM-GPG-KEY-oracle
+gpg --quiet --with-fingerprint /etc/pki/rpm-gpg/RPM-GPG-KEY-oracle
+
+echo '[ol7_latest]
+name=Oracle Linux $releasever Latest ($basearch)
+baseurl=https://yum.oracle.com/repo/OracleLinux/OL7/latest/$basearch/
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-oracle
+gpgcheck=1
+enabled=1' > /etc/yum.repos.d/ol7-temp
